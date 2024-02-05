9874b216f6ae88ee1b16251f45357249e865efec
Ikrom <74912914+Ikrom27@users.noreply.github.com>
2024-02-05 16:19:13 +0300

    navigation added

---
+++ app/src/main/java/com/ikrom/music_club_classic/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.ikrom.music_club_classic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val navView = findViewById<BottomNavigationView>(R.id.bottom_navigation_bar)
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+}
