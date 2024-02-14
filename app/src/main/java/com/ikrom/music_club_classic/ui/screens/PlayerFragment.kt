b9d35bf65f9861819949a4114a5d2674be7f5335
Ikrom <74912914+Ikrom27@users.noreply.github.com>
2024-02-15 00:59:27 +0300

    player fragment created

---
+++ app/src/main/java/com/ikrom/music_club_classic/ui/screens/PlayerFragment.kt
@@ -0,0 +1,89 @@
+package com.ikrom.music_club_classic.ui.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.ikrom.music_club_classic.R
+import com.ikrom.music_club_classic.extensions.togglePlayPause
+import com.ikrom.music_club_classic.playback.PlayerConnection
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class PlayerFragment : Fragment() {
+    @Inject
+    lateinit var playerConnection: PlayerConnection
+
+    private lateinit var trackCover: ImageView
+    private lateinit var trackTitle: TextView
+    private lateinit var trackAuthor: TextView
+    private lateinit var btnPlayPause: ImageButton
+    private lateinit var btnSkipNext: ImageButton
+    private lateinit var btnSkipPrevious: ImageButton
+    private lateinit var btnToFavorite: ImageButton
+    private lateinit var btnToRepeat: ImageButton
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_player, container, false)
+        bindViews(view)
+        setupContent()
+        setupButtons()
+        setupButtonsListener()
+        return view
+    }
+
+    private fun setupButtons() {
+        playerConnection.isPlaying.observe(viewLifecycleOwner) {
+            btnPlayPause.setImageResource(if (it) R.drawable.ic_pause else R.drawable.ic_play)
+        }
+    }
+
+    private fun setupButtonsListener() {
+        btnPlayPause.setOnClickListener {
+            playerConnection.player.togglePlayPause()
+        }
+        btnSkipNext.setOnClickListener {
+            playerConnection.player.seekToNext()
+        }
+        btnSkipPrevious.setOnClickListener {
+            playerConnection.player.seekToPrevious()
+        }
+        btnToFavorite.setOnClickListener {}
+
+        btnToRepeat.setOnClickListener {}
+    }
+
+    private fun bindViews(view: View){
+        trackCover = view.findViewById(R.id.iv_track_cover)
+        trackTitle = view.findViewById(R.id.tv_track_title)
+        trackAuthor = view.findViewById(R.id.tv_track_author)
+        btnPlayPause = view.findViewById(R.id.ib_play_pause)
+        btnSkipNext = view.findViewById(R.id.ib_skip_next)
+        btnSkipPrevious = view.findViewById(R.id.ib_skip_previous)
+        btnToFavorite = view.findViewById(R.id.ib_to_favorite)
+        btnToRepeat = view.findViewById(R.id.ib_to_repeat)
+    }
+
+
+    private fun setupContent() {
+        playerConnection.getCurrentMediaItem().observe(viewLifecycleOwner) {mediaItem ->
+            if (mediaItem != null) {
+                Glide
+                    .with(requireContext())
+                    .load(mediaItem.mediaMetadata.artworkUri)
+                    .into(trackCover)
+                trackTitle.text = mediaItem.mediaMetadata.title
+                trackAuthor.text = mediaItem.mediaMetadata.artist
+            }
+        }
+    }
+}
