1c4734ecc0a1b16c4f62c52bf6ececceb7bd423e
Ikrom <74912914+Ikrom27@users.noreply.github.com>
2024-03-21 00:15:58 +0300

    Album bar created

---
+++ app/src/main/java/com/ikrom/music_club_classic/ui/components/AlbumBar.kt
@@ -0,0 +1,65 @@
+package com.ikrom.music_club_classic.ui.components
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat
+import com.google.android.material.appbar.AppBarLayout
+import com.ikrom.music_club_classic.R
+
+class AlbumBar: AppBarLayout {
+    private lateinit var btnBack: ImageButton
+    private lateinit var btnSearch: ImageButton
+    private lateinit var btnMore: ImageButton
+
+    init {
+        this.addLiftOnScrollListener { elevation, backgroundColor ->
+            this@AlbumBar.setBackgroundColor(ContextCompat.getColor(context, R.color.bg_top_bar))
+        }
+    }
+
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(context)
+        getStuffFromXML(attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(context)
+        getStuffFromXML(attrs)
+    }
+
+    fun init(context: Context){
+        LayoutInflater.from(context).inflate(R.layout.component_album_bar, this, true)
+        btnBack = findViewById(R.id.btn_back)
+        btnMore = findViewById(R.id.btn_more)
+        btnSearch = findViewById(R.id.btn_search)
+    }
+
+    fun setOnBackClick(onClick: () -> Unit){
+        btnBack.setOnClickListener {
+            onClick()
+        }
+    }
+
+    fun setOnSearchClick(onClick: () -> Unit){
+        btnSearch.setOnClickListener {
+            onClick()
+        }
+    }
+
+    fun setOnMoreClick(onClick: () -> Unit){
+        btnMore.setOnClickListener {
+            onClick()
+        }
+    }
+
+    fun getStuffFromXML(attrs: AttributeSet?){
+        val data = context.obtainStyledAttributes(attrs, R.styleable.AppBar)
+    }
+}
--- app/src/main/java/com/ikrom/music_club_classic/ui/screens/PlaylistFragment.kt
+++ app/src/main/java/com/ikrom/music_club_classic/ui/screens/PlaylistFragment.kt
@@ -23,10 +23,12 @@ import com.ikrom.music_club_classic.data.model.Track
 import com.ikrom.music_club_classic.playback.PlayerHandler
 import com.ikrom.music_club_classic.ui.adapters.base_adapters.CompositeAdapter
 import com.ikrom.music_club_classic.ui.adapters.base_adapters.IDelegateItem
+import com.ikrom.music_club_classic.ui.adapters.base_adapters.item_decorations.MarginItemDecoration
 import com.ikrom.music_club_classic.ui.adapters.playlist.PlaylistHeaderDelegate
 import com.ikrom.music_club_classic.ui.adapters.playlist.PlaylistHeaderDelegateItem
 import com.ikrom.music_club_classic.ui.adapters.playlist.PlaylistTrackAdapter
 import com.ikrom.music_club_classic.ui.adapters.playlist.PlaylistTrackDelegateItem
+import com.ikrom.music_club_classic.ui.components.AlbumBar
 import com.ikrom.music_club_classic.viewmodel.PlayListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -42,9 +44,7 @@ class PlaylistFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var compositeAdapter: CompositeAdapter
-    private lateinit var btnBack: ImageButton
-    private lateinit var btnSearch: ImageButton
-    private lateinit var btnMore: ImageButton
+    private lateinit var albumBar: AlbumBar
     private lateinit var navController: NavController
 
 
@@ -56,8 +56,9 @@ class PlaylistFragment : Fragment() {
         currentPlaylist = viewModel.currentPlaylist.value
         trackList = viewModel.playlistItems
         navController = requireParentFragment().findNavController()
-        setupAdapter()
         bindViews(view)
+        setupAdapter()
+        setupRecyclerView()
         setupContent()
         setupButtons()
         return view
@@ -67,8 +68,14 @@ class PlaylistFragment : Fragment() {
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner){
             navController.navigateUp()
         }
-        btnBack.setOnClickListener{
+        albumBar.setOnBackClick {
             navController.navigateUp()
+        }
+        albumBar.setOnSearchClick {
+            //TODO: setOnSearchClick
+        }
+        albumBar.setOnMoreClick {
+           //TODO: setOnMoreClick
         }
     }
 
@@ -101,11 +108,23 @@ class PlaylistFragment : Fragment() {
 
     private fun bindViews(view: View){
         recyclerView = view.findViewById(R.id.rv_tracks)
+        albumBar = view.findViewById(R.id.album_bar)
+    }
+
+    private fun setupRecyclerView(){
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = compositeAdapter
-        btnBack = view.findViewById(R.id.btn_back)
-        btnMore = view.findViewById(R.id.btn_more)
-        btnSearch = view.findViewById(R.id.btn_search)
+        val playerHeight = resources.getDimensionPixelSize(R.dimen.mini_player_height)
+        val navbarHeight = resources.getDimensionPixelSize(R.dimen.bottom_nav_bar_height)
+        val appBarHeight =  resources.getDimensionPixelSize(R.dimen.app_bar_height)
+        if (recyclerView.itemDecorationCount == 0){
+            recyclerView.addItemDecoration(
+                MarginItemDecoration(
+                    startSpace = appBarHeight,
+                    endSpace = playerHeight + navbarHeight
+                )
+            )
+        }
     }
 
     private fun setupContent() {
