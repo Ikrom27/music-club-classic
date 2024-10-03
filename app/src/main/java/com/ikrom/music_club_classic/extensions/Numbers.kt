5a70b2d2674ee0eb591ab44127f01345c52c335d
ikrom <ikrom.hasanbaev.27@gmail.com>
2024-10-03 19:45:15 +0300

    remove unused code

--- app/src/main/java/com/ikrom/music_club_classic/extensions/Numbers.kt
+++
@@ -1,8 +0,0 @@
-package com.ikrom.music_club_classic.extensions
-
-import android.content.Context
-
-fun Int.toDp(context: Context): Int {
-    val density = context.resources.displayMetrics.density
-    return (this * density).toInt()
-}
