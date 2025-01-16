280e8f0765c752f12544deb14f0c9000e363f193
ikrom <ikrom.hasanbaev.27@gmail.com>
2025-01-17 01:19:05 +0300

    AlbumPageContent update

--- feature/album/src/main/java/ru/ikrom/album/AlbumPageContent.kt
+++ feature/album/src/main/java/ru/ikrom/album/AlbumPageContent.kt
@@ -3,7 +3,7 @@ package ru.ikrom.album
 import ru.ikrom.adapter_delegates.delegates.ThumbnailHeaderItem
 import ru.ikrom.adapter_delegates.delegates.ThumbnailSmallItem
 
-class AlbumPageContent(
+data class AlbumPageContent(
     val header: ThumbnailHeaderItem,
     val tracks: List<ThumbnailSmallItem>
 )
