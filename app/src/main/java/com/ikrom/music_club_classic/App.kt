42949b11e992f377a008f78ebc69db0035ecd316
ikrom <ikrom.hasanbaev.27@gmail.com>
2025-01-17 23:34:58 +0300

    only night mod

--- app/src/main/java/com/ikrom/music_club_classic/App.kt
+++ app/src/main/java/com/ikrom/music_club_classic/App.kt
@@ -1,6 +1,7 @@
 package com.ikrom.music_club_classic
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
@@ -8,6 +9,7 @@ import dagger.hilt.android.HiltAndroidApp
 class App: Application(){
     override fun onCreate() {
         DynamicColors.applyToActivitiesIfAvailable(this)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         super.onCreate()
     }
 }
